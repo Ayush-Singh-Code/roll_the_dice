3cd9b1e577ae8074ef6e66d48cf49940ba47b392
B.Jothin kumar <bjothinphysics@gmail.com>
2021-10-15 19:45:32 +0530

    GUI support added!

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import random
+import tkinter as tk
+from random import randint
 
-x = "y"
 
-while x == "y":
-	
-	# Gnenerates a random number
-	# between 1 and 6 (including
-	# both 1 and 6)
-	
-	print(random.randint(1,6))
-		
-	x=input("press y to roll again and n to exit:\n")
+# Creating Window
+root = tk.Tk()
+root.wm_title('Roll the dice!')  # Setting window title
+root.geometry('300x100')
+root.resizable(False, False)  # Window should not be resized
+
+# The widgets and functions
+dice_value_label = tk.Label(master=root, text='', fg='black', bg='snow')  # Tkinter label to display dice roll number.
+def roll_dice():
+	dice_value_label['text'] = str(randint(1, 6))
+roll_dice_button = tk.Button(master=root, text='Roll the dice!', fg='black', bg='snow', command=roll_dice)  # Button to roll dice.
+dice_value_label.pack()
+roll_dice_button.pack()
+
+root.mainloop()  # Calling mainloop!
